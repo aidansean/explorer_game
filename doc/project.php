5cf8a1a4ecfdda1b9f0d433e97d1c0d265f86113
aidansean <aidan.randleconde@gmail.com>
2015-08-02 20:41:28 +0200

    Added images, reorganised directory structure

--- doc/project.php
+++ doc/project.php
@@ -1,4 +1,6 @@
 <?php
+include_once($_SERVER['FILE_PREFIX']."/project_list/project_object.php") ;
 $github_uri   = "https://github.com/aidansean/explorer_game" ;
 $blogpost_uri = "http://aidansean.com/projects/?tag=explorer_game" ;
+$project = new project_object("explorer_game", "Room explorer game", "https://github.com/aidansean/explorer_game", "http://aidansean.com/projects/?tag=explorer_game", "explorer_game/images/project.jpg", "explorer_game/images/project_bw.jpg", "This project is a game where the player explores rooms with a top down view and does all the normal things, such as traverse mazes, open doors, fight monsters and so on.  This is a gravity-less version of the <a href=\"\">Platform game</a> and has an even more retro feel to it.  It's obviously a work in progress and needs a lot more work, but the main mechanics are complete and it even has its own editor.", "Games", "canvas,CSS,HTML,JavaScript") ;
 ?>
--- project.py
+++ project.py
@@ -3,10 +3,13 @@ from project_module import project_object, image_object, link_object, challenge_
 p = project_object('explorer_game', 'Room explorer game')
 p.domain = 'http://www.aidansean.com/'
 p.path = 'explorer_game'
-p.preview_image_ = image_object('http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg', 408, 287)
+p.preview_image    = image_object('%s/images/project.jpg'   %p.path, 150, 250)
+p.preview_image_bw = image_object('%s/images/project_bw.jpg'%p.path, 150, 250)
 p.folder_name = 'aidansean'
 p.github_repo_name = 'explorer_game'
 p.mathjax = False
+p.tags = 'Games'
+p.technologies = 'canvas,CSS,HTML,JavaScript'
 p.links.append(link_object(p.domain, p.path, 'Live page'))
 p.introduction = 'This project is a game where the player explores rooms with a top down view and does all the normal things, such as traverse mazes, open doors, fight monsters and so on.  This is a gravity-less version of the <a href="">Platform game</a> and has an even more retro feel to it.  It\'s obviously a work in progress and needs a lot more work, but the main mechanics are complete and it even has its own editor.'
 p.overview = '''The game works mostly through simple collision detection.  This differs from the platform game in that is has moving enemies that can be killed through shooting.  Given that it's in the development phase it's lacking any coherent narrative, but one day when I get time I'll come back to this game and add a lot more to it and make it much more interesting.
